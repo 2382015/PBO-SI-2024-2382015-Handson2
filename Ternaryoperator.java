08308ed2c4874f7e72e2d654e05678779fcfcc3f
Neliyulii <2382015@unai.edu>
2024-09-10 10:21:48 +0700

    handson2

---
+++ Ternaryoperator.java
@@ -0,0 +1,10 @@
+public class Ternaryoperator {
+    public static void main(String[] args) {
+        int nilaiLulusMinium = 70;
+        int nilaiMahasiswa = 70;
+        String hasil = nilaiMahasiswa >= nilaiLulusMinium ? "lulus" : "Tidak lulus";
+        if (nilaiMahasiswa >= nilaiLulusMinimum)
+            hasil = "Lulus";
+        } else {
+        hasil = "Tidak Lulus";
+    }
